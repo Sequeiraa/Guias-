8057af34b19441aaf5eaae58c2656170a35a2344
unknown <sequeirajosecarlos@gmail.com>
2021-02-23 17:56:22 -0600

    Ejercicio1

---
+++ Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.rbtn3 = new System.Windows.Forms.RadioButton();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Salario";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(72, 97);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(175, 20);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(328, 97);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(100, 20);
+            this.txtsalario.TabIndex = 3;
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.Location = new System.Drawing.Point(72, 167);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(63, 17);
+            this.rbtn1.TabIndex = 4;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "Gerente";
+            this.rbtn1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.Location = new System.Drawing.Point(212, 167);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(82, 17);
+            this.rbtn2.TabIndex = 5;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "SubGerente";
+            this.rbtn2.UseVisualStyleBackColor = true;
+            // 
+            // rbtn3
+            // 
+            this.rbtn3.AutoSize = true;
+            this.rbtn3.Location = new System.Drawing.Point(343, 167);
+            this.rbtn3.Name = "rbtn3";
+            this.rbtn3.Size = new System.Drawing.Size(84, 17);
+            this.rbtn3.TabIndex = 6;
+            this.rbtn3.TabStop = true;
+            this.rbtn3.Text = "Secretaria/o";
+            this.rbtn3.UseVisualStyleBackColor = true;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(212, 260);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 7;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 350);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.rbtn3);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.rbtn1);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.RadioButton rbtn3;
+        private System.Windows.Forms.Button btncalcular;
+    }
+}
+
---
+++ Ejercicio 1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                String nombre;
+                Double descuentos, salario, sueldoLiquido;
+
+                nombre = txtnombre.Text;
+                salario = Convert.ToDouble(txtsalario.Text);
+
+                if (rbtn1.Checked == true)
+                {
+                    descuentos = (salario * 20) / 100;
+                    sueldoLiquido = salario - descuentos;
+                    MessageBox.Show("Bienvenido " + nombre);
+                    MessageBox.Show("\nUsted es " + rbtn1.Text);
+                    MessageBox.Show("\nSu sueldo sin descuento es de " + salario);
+                    MessageBox.Show("\nEl descuento es de " + descuentos);
+                    MessageBox.Show("\nSu salario liquido es de " + sueldoLiquido);
+
+                }
+                if (rbtn2.Checked == true)
+                {
+                    descuentos = (salario * 15) / 100;
+                    sueldoLiquido = salario - descuentos;
+                    MessageBox.Show("Bienvenido " + nombre);
+                    MessageBox.Show("\nUsted es " + rbtn2.Text);
+                    MessageBox.Show("\nSu sueldo sin descuento es de " + salario);
+                    MessageBox.Show("\nEl descuento es de " + descuentos);
+                    MessageBox.Show("\nSu salario liquido es de " + sueldoLiquido);
+                }
+                if(rbtn3.Checked == true)
+                {
+                    descuentos = (salario * 5) / 100;
+                    sueldoLiquido = salario - descuentos;
+                    MessageBox.Show("Bienvenido " + nombre);
+                    MessageBox.Show("\nUsted es " + rbtn3.Text);
+                    MessageBox.Show("\nSu sueldo sin descuento es de " + salario);
+                    MessageBox.Show("\nEl descuento es de " + descuentos);
+                    MessageBox.Show("\nSu salario liquido es de " + sueldoLiquido);
+                }
+                if (rbtn1.Checked == false && rbtn2.Checked == false && rbtn3.Checked == false)
+                {
+                    MessageBox.Show("Debe estar marcada una casilla");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("No se aceptan campos de texto vacíos.");
+            }
+        }
+    }
+}
